d457168c11c2c5475539ec90e4a5e9ebbe295912
katia <ghilasmaouche89@gmail.com>
2023-07-14 10:27:31 +0200

    popover signin

--- components/Login.js
+++ components/Login.js
@@ -1,17 +1,22 @@
 import styles from '../styles/Login.module.css';
+import React, { useState } from 'react';
+import PopoverSignin from './Signin';
+import { Popover } from 'antd';
 
 
 function Login() {
 
-  const [isPopoverOpen, setIsPopoverOpen] = useState(false);
-  
-const handleSignupClick = () => {
-  setIsPopoverOpen(!isPopoverOpen);
-};
+  const [isPopoverInOpen, setIsPopoverInOpen] = useState(false);
 
-const handleSigninClick = () => {
-  setIsPopoverOpen(!isPopoverOpen);
-};
+  const handleSigninClick = () => {
+    setIsPopoverInOpen(!isPopoverInOpen);
+  };
+
+  
+// const handleSignupClick = () => {
+//   setIsPopoverOpen(!isPopoverOpen);
+// };
+
 
 
 // const handleClosePopup = () => {
@@ -35,12 +40,12 @@ const handleSigninClick = () => {
             <div>
               <h1 className={styles.h1}>See what's happening</h1>
               <h2 className={styles.h2}>Join Hackatweet today.</h2>
-              <button className={styles.buttonUp}onClick={handleSignupClick}>Signup</button>
+              <button className={styles.buttonUp}>Signup</button>
               <p className={styles.p}>Already have an account?</p>
               <button className={styles.buttonIn}onClick={handleSigninClick}>Signin</button>
-              {isPopoverOpen && (
-                <div className={styles.popover}>
-                  <Popover/> </div>)}
+              {isPopoverInOpen && (
+        <PopoverSignin  className={styles.Popover}/>
+      )}
             </div>
         </div> 
     
--- components/Signin.js
+++ components/Signin.js
@@ -1,47 +1,64 @@
 import React from 'react';
-import styles from '../styles/Login.module.css';
 import { useRouter } from 'next/router'
 import { Signin } from '../reducers/user';
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import styles from '../styles/sign.module.css';
 
 
-
-function Signin (){
-  const dispatch = useDispatch();
-
-  const [signInPassword, setSignInPassword] = useState('');
-  const [signInUsername, setSignInUSername] = useState('');
-  const router = useRouter();
-
- 
-    const handleLogin = () => {
-		fetch('http://localhost:3000/users/signin', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({username: signInUsername,password: signInPassword }),
-		}).then(response => response.json())
-			.then(data => {
-				if (data.result) {
-					dispatch(login({username: signInUsername,name: data.name, token:data.token})); // Ajoutez ici la logique d'inscription avec l'email et le mot de passe
-					setSignInemail('');
-					setSignInPassword('');}
-          router.push('/home')    // redirige vers la page Home
-				})
-			};
-	 
-
+const PopoverSignin = () => {
   return (
-    <div className={styles.loginContainer}>
-      <div className={styles.formContainer}>
-        <img src="logotweeter.png" alt="logo" className={styles.logoRight}/>
+    <div className={styles.popover}>
+      <span><FontAwesomeIcon icon={faXmark}   className={styles.crossIcon}/></span>
+        <img src="logotweeter.png" alt="logo" className={styles.logo}/>
         <h2>Connect to Hackatweet</h2>
-        <input type="text" placeholder="Username" onChange={(e) => setSignInUSername(e.target.value)} value={signInUsername}/>
-        <input type="password" placeholder="Password"onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword} />
-        <button onClick={handleLogin}>Sign up</button>
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        <button>Sign in</button>
+       
       </div>
-    </div>
   );
 };
 
-export default Signin;
+export default PopoverSignin;
+
+// function Signin (){
+//   const dispatch = useDispatch();
+
+//   const [signInPassword, setSignInPassword] = useState('');
+//   const [signInUsername, setSignInUSername] = useState('');
+//   const router = useRouter();
+
+ 
+//     const handleLogin = () => {
+// 		fetch('http://localhost:3000/users/signin', {
+// 			method: 'POST',
+// 			headers: { 'Content-Type': 'application/json' },
+// 			body: JSON.stringify({username: signInUsername,password: signInPassword }),
+// 		}).then(response => response.json())
+// 			.then(data => {
+// 				if (data.result) {
+// 					dispatch(login({username: signInUsername,name: data.name, token:data.token})); // Ajoutez ici la logique d'inscription avec l'email et le mot de passe
+// 					setSignInemail('');
+// 					setSignInPassword('');}
+//           router.push('/home')    // redirige vers la page Home
+// 				})
+// 			};
+	 
+
+//   return (
+//     <div className={styles.loginContainer}>
+//       <div className={styles.formContainer}>
+//         <img src="logotweeter.png" alt="logo" className={styles.logoRight}/>
+//         <h2>Connect to Hackatweet</h2>
+//         <input type="text" placeholder="Username" onChange={(e) => setSignInUSername(e.target.value)} value={signInUsername}/>
+//         <input type="password" placeholder="Password"onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword} />
+//         <button onClick={handleLogin}>Sign up</button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Signin;
